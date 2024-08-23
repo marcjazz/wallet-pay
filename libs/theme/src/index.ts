14d266c9d97c64295a238c76beb99b1eaf7da7a0
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-08-23 13:08:03 +0100

    feat(shared): SCRUM-9 generated theme library

--- libs/theme/src/index.ts
+++ libs/theme/src/index.ts
@@ -4,3 +4,4 @@ export * from './contexts/modeContext/ModeContextProvider';
 export * from './contexts/language/LanguageContextProvider';
 export * from './contexts/language/language.interface';
 export * from './contexts/user/user.provider';
+export * from './lib/theme';
