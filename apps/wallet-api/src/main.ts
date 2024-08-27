9c9598121dcb0fce6c3df8c1fb70385c48476a1c
Marco Kuidja <kuidjamarco@gmail.com>
2024-08-27 22:01:44 +0100

    feat: setup api versionning

--- apps/wallet-api/src/main.ts
+++ apps/wallet-api/src/main.ts
@@ -3,16 +3,24 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: {
+      origin: /^(?:[a-zA-Z0-9-]+\.)*xafpay\.com$/,
+      credentials: true,
+    },
+  });
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  app.enableVersioning({ type: VersioningType.URI, defaultVersion: '1' });
+  const port = process.env.PORT || 5000;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
