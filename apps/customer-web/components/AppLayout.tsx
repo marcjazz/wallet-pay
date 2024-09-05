c479ca1f7666294b2a2b02400bf8db7ca6c9716c
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2024-09-05 05:08:45 +0100

    feat: SCRUM-9 adjusted app layout to consume theme

--- apps/customer-web/components/AppLayout.tsx
+++ apps/customer-web/components/AppLayout.tsx
@@ -1,17 +1,7 @@
 'use client';
-import { responsiveFontSizes, ThemeProvider } from '@mui/material';
+import { XafpayThemeProvider } from '@xafpay/theme';
 import { FC, PropsWithChildren } from 'react';
-import { useTheme } from './theme';
 
 export const AppLayout: FC<PropsWithChildren> = ({ children }) => {
-  return (
-    <ThemeProvider
-      theme={responsiveFontSizes(useTheme(), {
-        factor: 50,
-        breakpoints: ['mobile', 'tablet', 'laptop', 'desktop'],
-      })}
-    >
-      {children}
-    </ThemeProvider>
-  );
+  return <XafpayThemeProvider>{children}</XafpayThemeProvider>;
 };
