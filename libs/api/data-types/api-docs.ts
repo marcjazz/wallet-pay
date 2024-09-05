e892b8438976d2fc8f9cb3e70d90e3c48df85409
Marco Kuidja <kuidjamarco@gmail.com>
2024-09-06 00:32:06 +0100

    docs: update api generated docs

--- libs/api/data-types/api-docs.ts
+++ libs/api/data-types/api-docs.ts
@@ -54,6 +54,22 @@ export interface paths {
         patch?: never;
         trace?: never;
     };
+    "/api/v1/auth/profile": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        get: operations["AuthController_getProfile"];
+        put?: never;
+        post?: never;
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
 }
 export type webhooks = Record<string, never>;
 export interface components {
@@ -63,6 +79,35 @@ export interface components {
             email: string;
             /** @description Strong password */
             password: string;
+        };
+        AuthTokensDto: Record<string, never>;
+        SignUpDto: {
+            /** @description Valid user email */
+            email: string;
+            /** @description Strong password */
+            password: string;
+            /** @enum {string} */
+            country: "USA" | "CANADA";
+            username: string;
+            /** @description User first name */
+            first_name: string;
+            /** @description User last name */
+            last_name: string;
+            /** @description Valid user phone number */
+            phone_number: string;
+            /**
+             * Format: date-time
+             * @description User date of birth
+             */
+            birthdate: string;
+            /**
+             * @description User gender
+             * @example MALE
+             * @enum {string}
+             */
+            gender: "MALE" | "FEMALE" | "OTHER";
+            /** @enum {string} */
+            preferred_language: "EN_US" | "FR";
         };
         PersonEntity: {
             /** @description Valid user email */
@@ -95,42 +140,14 @@ export interface components {
             preferred_language: "EN_US" | "FR";
             /**
              * @description User person id
-             * @example 3eee38fd-d89a-4aea-be50-bdb8ec63d145
+             * @example e34e5d06-0a1a-4114-94e3-a1c25c53407e
              */
             person_id: string;
             /**
              * @description Account creation datetime in milleseconds.
-             * @example 1725521822685
+             * @example 1725578969950
              */
             created_at: number;
-        };
-        SignUpDto: {
-            /** @description Valid user email */
-            email: string;
-            /** @description Strong password */
-            password: string;
-            /** @enum {string} */
-            country: "USA" | "CANADA";
-            username: string;
-            /** @description User first name */
-            first_name: string;
-            /** @description User last name */
-            last_name: string;
-            /** @description Valid user phone number */
-            phone_number: string;
-            /**
-             * Format: date-time
-             * @description User date of birth
-             */
-            birthdate: string;
-            /**
-             * @description User gender
-             * @example MALE
-             * @enum {string}
-             */
-            gender: "MALE" | "FEMALE" | "OTHER";
-            /** @enum {string} */
-            preferred_language: "EN_US" | "FR";
         };
     };
     responses: never;
@@ -176,7 +193,7 @@ export interface operations {
                     [name: string]: unknown;
                 };
                 content: {
-                    "application/json": components["schemas"]["PersonEntity"];
+                    "application/json": components["schemas"]["AuthTokensDto"];
                 };
             };
             /** @description Bad request. This often happens when the request payload it not respected. */
@@ -227,7 +244,7 @@ export interface operations {
                     [name: string]: unknown;
                 };
                 content: {
-                    "application/json": components["schemas"]["PersonEntity"];
+                    "application/json": components["schemas"]["AuthTokensDto"];
                 };
             };
             /** @description Bad request. This often happens when the request payload it not respected. */
@@ -239,6 +256,39 @@ export interface operations {
             };
             /** @description Conflict, user email is already registered with another account. */
             409: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content?: never;
+            };
+            /** @description Internal server error. An unexpected exception was thrown */
+            500: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content?: never;
+            };
+        };
+    };
+    AuthController_getProfile: {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        requestBody?: never;
+        responses: {
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "application/json": components["schemas"]["PersonEntity"];
+                };
+            };
+            /** @description Bad request. This often happens when the request payload it not respected. */
+            400: {
                 headers: {
                     [name: string]: unknown;
                 };
