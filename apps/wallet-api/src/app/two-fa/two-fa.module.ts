ad8a7d6a9bf813f20b5fe48cecd83b2e5017d539
Marco Kuidja <kuidjamarco@gmail.com>
2024-09-08 21:34:46 +0100

    fix: remove mailer here as it's now declared globally and already imported in `app.module.ts`

--- apps/wallet-api/src/app/two-fa/two-fa.module.ts
+++ apps/wallet-api/src/app/two-fa/two-fa.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { OTPService } from './otp/otp.service';
 import { OTPController } from './otp/otp.controller';
-import { MailerModule } from '../../mailer/mailer.module';
+import { OTPService } from './otp/otp.service';
 
 @Module({
-  imports: [MailerModule],
   controllers: [OTPController],
   providers: [OTPService],
   exports: [OTPService],
