18ed3468c4ac1bdf3d4b8117f01935225367aaa1
Marco Kuidja <kuidjamarco@gmail.com>
2024-09-13 07:10:01 +0100

    fix: adjusted cybrid module importation

--- apps/wallet-api/src/app/auth/auth.module.ts
+++ apps/wallet-api/src/app/auth/auth.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { CybridDynamicModule } from '../../cybrid/cybrid.module';
 import { TwoFAModule } from '../two-fa/two-fa.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt/jwt.strategy';
 import { LocalStrategy } from './local/local.strategy';
 import { RolesService } from './roles.service';
-import { CybridModule } from '../../cybrid/cybrid.module';
 
 @Module({
   imports: [
@@ -16,10 +16,7 @@ import { CybridModule } from '../../cybrid/cybrid.module';
       secret: process.env.JWT_SECRET,
     }),
     TwoFAModule,
-    CybridModule.forRoot({
-      username: process.env.CYBRID_CLIENT_ID,
-      password: process.env.CYBRID_CLIENT_SECRET,
-    }),
+    CybridDynamicModule,
   ],
   providers: [AuthService, RolesService, JwtStrategy, LocalStrategy],
   controllers: [AuthController],
