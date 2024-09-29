972e3043d6993fa194e06ff5ab363956fad62ddc
Tchakoumi <lorraintchakoumi@gmail.com>
2024-09-29 13:23:32 +0100

    chore (shared): SCRUM-9 removed unused import

--- libs/theme/src/xafpayThemeProvider.tsx
+++ libs/theme/src/xafpayThemeProvider.tsx
@@ -1,4 +1,4 @@
-import { CssBaseline, responsiveFontSizes, ThemeProvider } from '@mui/material';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
