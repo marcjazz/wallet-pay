f2e717bb011caff68906c370d21cf0ab480210fe
Tchakoumi <lorraintchakoumi@gmail.com>
2024-09-30 14:49:55 +0100

    feat (customer-web): SCRUM-28 added the footer to the page

--- apps/customer-web/app/page.tsx
+++ apps/customer-web/app/page.tsx
@@ -6,6 +6,7 @@ import MainCard from '../components/Home/MainCard';
 import TransactionSection from '../components/Home/TransactionSection';
 import Footer from '../components/layout/footer/Footer';
 import Header from '../components/layout/header/Header';
+import WelcomeScreen from '../components/layout/WelcomeScreen';
 import TransactionHistory from '../components/transaction/TransactionHistory';
 
 export default function Index() {
@@ -13,11 +14,11 @@ export default function Index() {
 
   return (
     <>
+      <WelcomeScreen />
       <TransactionHistory
         isMenuOpen={isAllHistoryOpen}
         handleClose={() => setIsAllHistoryOpen(false)}
       />
-
       <Box
         sx={{
           padding: 2,
