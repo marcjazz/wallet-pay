e470e4dd23b0c6f2846f0bd94827d2e913104a7f
Tchakoumi <lorraintchakoumi@gmail.com>
2024-11-11 14:36:49 +0100

    fix(customer-web): adjusted intl locale to have correct date/times

--- libs/theme/src/xafpayThemeProvider.tsx
+++ libs/theme/src/xafpayThemeProvider.tsx
@@ -2,7 +2,7 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { IntlProvider } from 'react-intl';
 import { Bounce, ToastContainer } from 'react-toastify';
 import LanguageContextProvider, {
@@ -16,11 +16,17 @@ import UserContextProvider from './contexts/user/user.provider';
 import enMessages from './languages/en-us';
 import frMessages from './languages/fr';
 import { useTheme } from './theme';
+import { LanguageType } from './contexts/language/language.interface';
 
 const TempApp = ({ children }: { children: React.ReactNode }) => {
   const { modeDispatch } = useMode();
   const { activeLanguage } = useLanguage();
   const activeMessages = activeLanguage === 'En' ? enMessages : frMessages;
+  // const supportedLocales: Record<LanguageType, string> = {
+  //   En: 'en-US',
+  //   Fr: 'fr-Fr',
+  // };
+  const [activeLocale, setActiveLocale] = useState('en-US');
 
   useEffect(() => {
     const isSystemInDarkMode = window.matchMedia(
@@ -31,14 +37,17 @@ const TempApp = ({ children }: { children: React.ReactNode }) => {
         ? 'dark'
         : 'light';
     modeDispatch({ type: mode === 'dark' ? 'USE_DARK' : 'USE_LIGHT' });
+    // TODO: see how to match locales with supported locales and initiate it.
+    setActiveLocale(navigator.language);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <IntlProvider
       messages={activeMessages}
-      locale={activeLanguage}
-      defaultLocale="En"
+      // TODO:
+      locale={activeLocale}
+      defaultLocale="en-US"
     >
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <UserContextProvider>
