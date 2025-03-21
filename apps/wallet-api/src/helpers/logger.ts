5413058e4dc11d326ca8b9bc3737fc46e0c06520
Marco Kuidja <marco.kuidja@adorsys.com>
2025-03-21 21:13:42 +0100

    chore(logger): logged user agent and romote address

--- apps/wallet-api/src/helpers/logger.ts
+++ apps/wallet-api/src/helpers/logger.ts
@@ -1,9 +1,9 @@
 import { Logger } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 
-export function logger(req: Request, res: Response, next: NextFunction) {
+export function logger(req: Request, _res: Response, next: NextFunction) {
   Logger.log(
-    `${req.originalUrl} (origin: ${req.headers.origin}, client IP: ${req.ip})`,
+    `${req.originalUrl} (User agent: ${req.headers['user-agent']}, client IP: ${req.socket.remoteAddress})`,
     req.method
   );
   next();
