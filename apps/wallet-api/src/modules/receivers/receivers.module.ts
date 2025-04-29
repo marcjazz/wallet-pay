6069617215278ff1729b36b9e15feeb51751d2ee
Marco Kuidja <kuidjamarco@gmail.com>
2025-04-29 19:33:30 +0100

    refactor(receivers): remove Peex service integration and unused test endpoint

--- apps/wallet-api/src/modules/receivers/receivers.module.ts
+++ apps/wallet-api/src/modules/receivers/receivers.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { CybridModule } from '../../cybrid/cybrid.module';
 import { ReceiversController } from './receivers.controller';
 import { RecieversService } from './receivers.service';
-import { PeexService } from '@xafpay/api/peex/peex.service';
 
 @Module({
   imports: [CybridModule],
   controllers: [ReceiversController],
-  providers: [RecieversService, PeexService],
+  providers: [RecieversService],
   exports: [RecieversService],
 })
 export class ReceiversModule {}
