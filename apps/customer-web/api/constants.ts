ab5d36d1d05761cbdbde5ac8c5b03c48caaa9b69
Marco Kuidja <kuidjamarco@gmail.com>
2025-05-10 16:21:40 +0100

    chore(constants): update API_BASE_URL to use production endpoint instead of localhost

--- apps/customer-web/api/constants.ts
+++ apps/customer-web/api/constants.ts
@@ -1,2 +1,2 @@
 //TODO: REMOVE LOCAL LINK
-export const API_BASE_URL = process.env.NX_PUBLIC_API_BASE_URL ?? 'http://localhost:8080';
+export const API_BASE_URL = process.env.NX_PUBLIC_API_BASE_URL ?? 'https://api.xafpay.com';
