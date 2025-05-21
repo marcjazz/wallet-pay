f89f03a6ea0c2823f2ae4b36f08a8f0d262634cc
Tchakoumi <lorraintchakoumi@gmail.com>
2025-05-21 12:41:35 +0200

    feat: adjusted validatePhoneNumber to validate w/out country code

--- apps/wallet-api/src/helpers/utils.ts
+++ apps/wallet-api/src/helpers/utils.ts
@@ -13,10 +13,10 @@ export function roundNumber(val: number): number {
 export function validatePhoneNumber(phoneNumber: string) {
   const testCase = phoneNumber.replace('+', '');
   const mtnRegexp = new RegExp(
-    /^[1-9]{1,3}6(((7|8)[0-9]{7}$)|(5[1-4][0-9]{6}$))/
+    /^([1-9]{1,3})?6(((7|8)[0-9]{7}$)|(5[1-4][0-9]{6}$))/
   );
   const orangeRegexp = new RegExp(
-    /^[1-9]{1,3}6(((9)[0-9]{7}$)|(5[5-9][0-9]{6}$))/
+    /^([1-9]{1,3})?6(((9)[0-9]{7}$)|(5[5-9][0-9]{6}$))/
   );
   if (mtnRegexp.test(testCase)) return 0;
   else if (orangeRegexp.test(testCase)) return 1;
