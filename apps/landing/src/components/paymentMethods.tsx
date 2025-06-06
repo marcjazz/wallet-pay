a707af8a988430cbb75f63754ee988cc34304f61
Poukam-Pierre <ngamaleu2011@gmail.com>
2025-06-06 16:26:00 +0100

    feat(app): #29 add Cta, FAQ, and PaymentMethods  as components with responsive design

---
+++ apps/landing/src/components/paymentMethods.tsx
@@ -0,0 +1,63 @@
+import { Box, Typography } from "@mui/material";
+import { useTheme } from "@xafpay/theme";
+import { IPaymentMethods } from "@xafpay/types";
+import Image from "next/image";
+import { useIntl } from "react-intl";
+import SectionTitle from "./sectionTitle";
+
+
+export default function PaymentMethods() {
+    const { formatMessage } = useIntl();
+    const theme = useTheme();
+
+
+    const paymentMethods: IPaymentMethods[] = [
+        {
+            link: '/assets/momo.png',
+            alt: 'MTN mobile money'
+        },
+        {
+            link: '/assets/om.png',
+            alt: 'Orange money'
+        },
+        {
+            link: '/assets/cash.png',
+            alt: 'Cash money'
+        },
+        {
+            link: '/assets/bank-deposit.png',
+            alt: 'Bank deposit'
+        },
+    ]
+
+    return (
+        <Box sx={{
+            display: 'grid',
+            rowGap: 8,
+            margin: 7,
+            width: '100%',
+        }}>
+            <SectionTitle color="#0E103A">
+                {formatMessage({ id: 'payoutMethods' })}
+            </SectionTitle>
+            <Box sx={{
+                display: 'grid',
+                gridAutoFlow: 'column',
+                columnGap: 7,
+                width: 'fit-content',
+                justifySelf: 'center',
+            }}>
+                {paymentMethods.map(({ link, alt }, index) => (
+                    <Image
+                        key={index}
+                        src={link}
+                        alt={alt}
+                        width={100}
+                        height={60}
+                    />
+                ))}
+            </Box>
+        </Box>
+
+    )
+}
