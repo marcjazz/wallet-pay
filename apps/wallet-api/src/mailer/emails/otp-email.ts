e7eafa053de94d89fad8292a46110c985aa871e0
Poukam-Pierre <ngamaleu2011@gmail.com>
2025-07-07 21:26:54 +0100

    feat(app): #56 Add expiration OTP time and highlight them.

--- apps/wallet-api/src/mailer/emails/otp-email.ts
+++ apps/wallet-api/src/mailer/emails/otp-email.ts
@@ -17,6 +17,7 @@ export function generateOtpCodeEmail(data: ConfirmEmail) {
         <div style="padding: 20px; text-align: center;">
           <p style="font-size: 16px; color: #666;">One time password from XafPay</p>
           <p style="font-size: 16px; color: #666;">Please copy and paste the above code in your XafPay application</p>
+          <p style="font-size: 16px; color: #666;">This code has <strong>5 minutes</strong> validity period.</p>
         </div>
         <div style="margin-top: 20px; text-align: center; padding: 10px 0; background-color: #f4f4f4; border-radius: 5px;">
             <p>If you have any questions, feel free to contact our support team at <a href="mailto:contact@xafpay.com">contact@xafpay.com</a>.</p>
