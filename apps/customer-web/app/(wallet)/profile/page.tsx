29103bd1214c55ae1a517c452077ba7ba2bf5218
Poukam-Pierre <ngamaleu2011@gmail.com>
2025-07-20 18:06:54 +0100

    fix(app): #72  Update profile layout by adding scrollbar.

--- apps/customer-web/app/(wallet)/profile/page.tsx
+++ apps/customer-web/app/(wallet)/profile/page.tsx
@@ -10,6 +10,7 @@ import Footer from '../../../components/layout/footer/Footer';
 import ProfileEdit from '../../../components/profile/ProfileEdit';
 import ProfileOverview from '../../../components/profile/ProfileOverview';
 import Header from '../../../components/shared/header';
+import Scrollbars from 'rc-scrollbars';
 
 interface ITabPanel {
   label: string;
@@ -56,56 +57,55 @@ export default function ProfilePage() {
         sx={{
           padding: 2,
           display: 'grid',
-          gridTemplateRows: 'auto 1fr',
+          gridTemplateRows: 'auto auto 1fr',
           rowGap: 5,
           overflow: 'auto'
         }}
       >
         <Header label="profile" />
-        <Box>
-          <Tabs
-            centered
-            value={activeTab}
-            onChange={(_, tabIndex) => setActiveTab(tabIndex)}
+        <Tabs
+          centered
+          value={activeTab}
+          onChange={(_, tabIndex) => setActiveTab(tabIndex)}
+          sx={{
+            '& .MuiTabs-indicator': {
+              display: 'none' // Hide default indicator
+            },
+            '& .MuiTabs-flexContainer': {
+              gap: '10px'
+            },
+            bgcolor: 'rgba(190, 197, 197, 0.3)',
+            borderRadius: '10px'
+          }}
+        >
+          {tabsLabel.map(({ label, tabIcon }, index) => (
+            <Tab
+              key={`tab-${index}`}
+              label={label}
+              icon={tabIcon}
+              iconPosition="start"
+              sx={{
+                textTransform: 'none'
+              }}
+            />
+          ))}
+        </Tabs>
+        {isUserProfileLoading ? (
+          <Box
             sx={{
-              '& .MuiTabs-indicator': {
-                display: 'none' // Hide default indicator
-              },
-              '& .MuiTabs-flexContainer': {
-                gap: '10px'
-              },
-              bgcolor: 'rgba(190, 197, 197, 0.3)',
-              borderRadius: '10px',
-              marginBottom: '32px'
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '70%'
             }}
           >
-            {tabsLabel.map(({ label, tabIcon }, index) => (
-              <Tab
-                key={`tab-${index}`}
-                label={label}
-                icon={tabIcon}
-                iconPosition="start"
-                sx={{
-                  textTransform: 'none'
-                }}
-              />
-            ))}
-          </Tabs>
-          {isUserProfileLoading ? (
-            <Box
-              sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '70%'
-              }}
-            >
-              <CircularProgress thickness={23} color="primary" />
-            </Box>
-          ) : (
-            profileTabs[activeTab]
-          )}
-        </Box>
+            <CircularProgress thickness={23} color="primary" />
+          </Box>
+        ) : (
+          <Scrollbars universal autoHide>
+            {profileTabs[activeTab]}
+          </Scrollbars>
+        )}
       </Box>
       <Footer />
     </Box>
