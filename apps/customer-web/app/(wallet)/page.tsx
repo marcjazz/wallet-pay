7f808d829febd2293eb24373a8c5bb182a98d9bc
Marco Kuidja <kuidjamarco@gmail.com>
2025-08-04 02:04:28 +0100

    fix: updated welcome screen logic

--- apps/customer-web/app/(wallet)/page.tsx
+++ apps/customer-web/app/(wallet)/page.tsx
@@ -1,20 +1,33 @@
 'use client';
 
 import { Box } from '@mui/material';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import MainCard from '../../components/Home/MainCard';
 import TransactionSection from '../../components/Home/TransactionSection';
 import Footer from '../../components/layout/footer/Footer';
 import Header from '../../components/layout/header/Header';
 import TransactionHistory from '../../components/transaction/TransactionHistory';
-import WelcomeScreenManager from '../../components/WelcomeScreenManager';
+import WelcomeScreen from '../../components/layout/WelcomeScreen';
 
 export default function Index() {
   const [isAllHistoryOpen, setIsAllHistoryOpen] = useState<boolean>(false);
+  const [shouldShowWelcomeScreen, setShouldShowWelcomeScreen] = useState(true);
 
-  return (
+  useEffect(() => {
+    setShouldShowWelcomeScreen(
+      !localStorage.getItem('shouldShowWelcomeScreen')
+    );
+  }, []);
+
+  const handleWelcomeScreenSwipe = () => {
+    setShouldShowWelcomeScreen(false);
+    localStorage.setItem('shouldShowWelcomeScreen', 'false');
+  };
+
+  return shouldShowWelcomeScreen ? (
+    <WelcomeScreen handleSwipe={handleWelcomeScreenSwipe} />
+  ) : (
     <>
-      <WelcomeScreenManager />
       <TransactionHistory
         isMenuOpen={isAllHistoryOpen}
         handleClose={() => setIsAllHistoryOpen(false)}
